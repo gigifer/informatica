0e4240ebbb063668580a748b232b83aa7962fee8
Gisela <fernandezgisela27@gmail.com>
2019-12-09 00:55:57 -0300

    header responsive,agrego footer y logo nuevo

--- header.php
+++ header.php
@@ -2,47 +2,47 @@
   <div class="row" id="buscador">
     <div class="nombre text-white col-sm-12 col-md-5 col-lg-5">
       <img src="Logo-Mapache.png" alt="logo">
-      MAPACHE
-  </div>
-
-    <nav class="navbar col-sm-12 col-md-5 col-lg-5"  >
-      <form class="form-inline" >
-        <input class="form-control mr-sm-2" type="search" aria-label="Search">
-        <button id= "botBuscar" class="btn btn-outline-success my-2 my-sm-0 " type="submit">Buscar</button>
+      Mapache
+    </div>
+    <div class="buscar col-sm-12 col-md-5 col-lg-5">
+      <form class="form-inline d-flex justify-content-center" >
+        <input class="rounded-pill" type="search" aria-label="Search">
+        <button id= "botBuscar" class="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
       </form>
-    </nav>
-    <div class="carrito col-sm-12 col-md-2 col-lg-2">
-      <i class="fas fa-cart-arrow-down"></i>
-      <div>
-        <a id="misCompras" class="text-white" href="#">Mis compras</a>
+    </div>
+
+    <div class="col-sm-12 col-md-2 col-lg-2">
+      <div class="carrito">
+        <i class="fas fa-cart-arrow-down"></i>
+      </div>
+      <div id="misCompras">
+        <a class="text-white" href="#">Mis compras</a>
       </div>
     </div>
   </div>
 
-  <nav class="navbar navbar-expand-lg row navbar-light" id="barraNav">
-    <div class="collapse navbar-collapse col-lg-12" id="navbarNavDropdown">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item active itemMenu col-sm-12 col-md-3 col-lg-3">
-          <a class="nav-link text-white" href="#">Home <span class="sr-only">(current)</span></a>
+    <div class="row">
+      <ul class="nav container-fluid" id=barraNav>
+        <li class="nav-item text-center text-white col-sm-12 col-md-3 col-lg-3">
+        <a class="nav-link active text-white" href="#">Home</a>
         </li>
-        <li class="nav-item dropdown itemMenu col-sm-12 col-md-3 col-lg-3">
-          <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Productos
-          </a>
-          <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item " href="#">Monitores</a>
-            <a class="dropdown-item" href="#">Notebooks</a>
-            <a class="dropdown-item" href="#">Gabinetes</a>
-            <a class="dropdown-item" href="#">Accesorios</a>
-          </div>
-        </li>
-        <li class="nav-item itemMenu col-sm-12 col-md-3 col-lg-3">
-          <a class="nav-link text-white" href="#">Registrate</a>
-        </li>
-        <li class="nav-item itemMenu col-sm-12 col-md-3 col-lg-3">
-          <a class="nav-link text-white" href="#">Ingresá</a>
-        </li>
-      </ul>
+      <li class="nav-item dropdown text-center col-sm-12 col-md-3 col-lg-3">
+        <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Productos
+        </a>
+        <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item " href="#">Monitores</a>
+          <a class="dropdown-item" href="#">Notebooks</a>
+          <a class="dropdown-item" href="#">Gabinetes</a>
+          <a class="dropdown-item" href="#">Accesorios</a>
+        </div>
+      </li>
+      <li class="nav-item text-center col-sm-12 col-md-3 col-lg-3">
+        <a class="nav-link active text-white" href="#">Registrate</a>
+      </li>
+      <li class="nav-item text-center col-sm-12 col-md-3 col-lg-3">
+        <a class="nav-link active text-white" href="#">Ingresá</a>
+      </li>
+    </ul>
     </div>
-  </nav>
 </header>
