d3bd3fea4206cc758f03791e2b4ad9b281de7429
MarCaro <caropompolomaria@gmail.com>
2019-12-12 19:11:40 -0300

    Agrego productos, formulario de usuarios y perfil del usuario

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
       <link rel="stylesheet" href="style.css">
       <link href="https://fonts.googleapis.com/css?family=Playfair+Display|Righteous&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Gelasio&display=swap" rel="stylesheet">
       <title>Mapache</title>
       <script src="https://kit.fontawesome.com/acf02b5d89.js" crossorigin="anonymous"></script>
     </head>
 
     <body>
-      <div class="container-fluid">
+
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -43,10 +44,82 @@
           <span class="sr-only">Next</span>
         </a>
       </div>
+      <!-- productos -->
+      <section id= "productos" class="container-fluid">
+        <div class="row">
+      <article class="col-sm-12 col-md-6 col-lg-6 producto">
+        <img src="img/notebook1.jpg" alt="">
+        <h2>Notebook Acer</h2>
+        <p>
+          Notebook 14-cm0045la Amd A4 Ram 4gb Dd 64gb Radeon R3 W10
+          Sistema operativo:Windows 10 Home en modo S
+          Memoria (RAM) 4GB
+          Procesador AMD Dual-Core serie
+          Tamaño de pantalla: 14"
+        </p>
+        <a  href="#"><button>VER MAS</button></a>
+      </article>
 
-      <?php include ("footer.php") ?>
-      </div>
+    <article class="col-sm-12 col-md-6 col-lg-6 producto">
+      <img src="img/notebook2.jpg" alt="">
+      <h2>Notebook CX</h2>
+      <p>
+        • Pantalla: 11.6” - Formato 16:9
+        • Procesador: Intel Atom x5 - Z8350 1.44 Ghz
+        • Memoria RAM: 2GB DDR3L
+        • Almacenamiento: 32GB
+        • Gráficos: Gen8 - LP10 / 12E Up to 600MHz
+        • Puertos: Mini HDMI Type C 1.4 / x 1 USB 3.0 / x1 USB 2.0 Soporta SDHC / SDXC
+        • Conectividad: WIFI802.11 norma/b/g/n + BT
+        • Sistema operativo: Windows 10 Home
+      </p>
+      <a href="#"><button>VER MAS</button></a>
+    </article>
+
+  <article class="col-sm-12 col-md-6 col-lg-6 producto">
+    <img src="img/pc1.jpg" alt="">
+    <h2>PC completa hogar</h2>
+    <p>
+      Rendimiento: Pc de escritorio equipada con un potente Procesador Intel Core i5, 8GB de memoria RAM (expansible a 32GB) y almacenamiento SSD de 240GB, podrás ejecutar programas en simultáneo sin necesidad de esperas prolongadas y alternar su uso en tiempo real.
+    </p>
+    <a href="#"><button>VER MAS</button></a>
+  </article>
+
+  <article class="col-sm-12 col-md-6 col-lg-6 producto">
+    <img src="img/pc2.jpg" alt="">
+    <h2>Pc completa oficina</h2>
+    <p>
+    PC ideal para disfrutar de tus actividades diarias de ocio y de oficina. Cuenta con un poderoso micro procesador capaz de brindar un rendimiento óptimo en aplicaciones de todo tipo e inclusive disfrutar de algunos videojuegos. Ver películas, navegar por Internet, almacenar documentos, editar tus fotos, disfrutar de juegos, escuchar tu música y más nunca fue tan simple ¡EQUIPA TU HOGAR O TU OFICINA!
+    </p>
+    <a href="#"><button>VER MAS</button></a>
+  </article>
+  <article class="col-sm-12 col-md-6 col-lg-6 producto">
+    <img src="img/teclado1.jpg" alt="">
+    <h2>Teclado Logitec</h2>
+    <p>
+      Este teclado de 87 teclas brinda todo lo que un jugador profesional exige, sin necesidad del teclado numérico. Alloy FPS Pro cuenta con los interruptores mecánicos con cables con gran capacidad de respuesta CHERRY® MX, y su resistente estructura de acero sólido te asegura que tendrás una plataforma estable durante los momentos más importantes de tu juego.
+    </p>
+    <a href="#"><button>VER MAS</button></a>
+  </article>
+  <article class="col-sm-12 col-md-6 col-lg-6 producto">
+    <img src="img/teclado2.jpg" alt="">
+    <h2>Teclado Razer</h2>
+    <p>Este teclado alto rendimiento hará que disfrutes de horas ilimitadas de juego. Está diseñado especialmente para que puedas expresar tanto tus habilidades como tu estilo. Mejorá muchísimo tu experiencia de gaming, ya seas un aficionado o todo un experto, y hacé que tus jugadas alcancen otro nivel.
+
+    </p>
+    <a href="#"><button>VER MAS</button></a>
+  </article>
 
 
+
+</section>
+    <!-- fin Productos -->
+
+
+
+
+
+</div>
+  <?php include ("footer.php") ?>
     </body>
   </html>
