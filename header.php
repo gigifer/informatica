8c6c708b57d999bc385b2a3d3b40a4c4c978a3f7
Gisela <fernandezgisela27@gmail.com>
2019-12-16 21:02:24 -0300

    footer y header modificados para sacar barra de scroll

--- header.php
+++ header.php
@@ -1,31 +1,30 @@
 <header>
   <div class="container-fluid">
+    <div class="row" id="buscador">
+      <div class="nombre text-white col-sm-12 col-md-5 col-lg-5 ">
 
-  <div class="row" id="buscador">
-    <div class="nombre text-white col-sm-12 col-md-5 col-lg-5 ">
-
-      <a href="index.php">
-      <img id="logodepagina" src="Logo-Mapache.png" alt="logo">
-      <h1 id= "titulodepagina">Mapache</h1></a>
-    </div>
-    <div class="buscar col-sm-12 col-md-5 col-lg-5">
-      <form class="form-inline d-flex justify-content-center" >
-        <input class="rounded-pill" type="search" aria-label="Search" id="inputBuscador">
-        <button id= "botBuscar" class="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
-      </form>
-    </div>
-
-    <div class="col-sm-12 col-md-2 col-lg-2">
-      <div class="carrito">
-          <a href="carrito.php"><i class="fas fa-cart-arrow-down"></i></a>
+        <a href="index.php">
+        <img id="logodepagina" src="Logo-Mapache.png" alt="logo">
+        <h1 id= "titulodepagina">Mapache</h1></a>
       </div>
-      <div id="misCompras">
-        <a class="text-white" href="carrito.php">Mis compras</a>
+      <div class="buscar col-sm-12 col-md-5 col-lg-5">
+        <form class="form-inline d-flex justify-content-center" >
+          <input class="rounded-pill" type="search" aria-label="Search" id="inputBuscador">
+          <button id= "botBuscar" class="btn btn-outline-success rounded-pill" type="submit">Buscar</button>
+        </form>
+      </div>
+
+      <div class="col-sm-12 col-md-2 col-lg-2">
+        <div class="carrito">
+            <a href="carrito.php"><i class="fas fa-cart-arrow-down"></i></a>
+        </div>
+        <div id="misCompras">
+          <a class="text-white" href="carrito.php">Mis compras</a>
+        </div>
       </div>
     </div>
-  </div>
 
-    <div class="row">
+    <div class="row" id="navSinMargen">
       <ul class="nav container-fluid" id=barraNav>
         <li class="nav-item text-center text-white col-sm-12 col-md-3 col-lg-3">
         <a class="nav-link active text-white" href="index.php">Home</a>
