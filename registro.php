6f5d095afa74624cd2d1ccb8fe57d68ace10e503
LeoLazc <lazcano.leonel@gmail.com>
2019-12-23 22:57:54 -0300

    validacion de registro, files y json

--- registro.php
+++ registro.php
@@ -1,67 +1,119 @@
 <?php
-      //conservar datos ingresados por el usuario (persistencia)
-      $usuario = "";
-      $email = "";
-      $contraseña = "";
-      $repetirContraseña = "";
+$errores = array();
+$usuario = "";
+$email = "";
+$contraseña = "";
 
-
-    if(($_POST)){
-
-      $usuario = $_POST["usuario"];
-      $email = $_POST["email"];
-      $contraseña = $_POST["contraseña"];
-      $repetirContraseña = $_POST["repetirContraseña"];
-    }
-
-    //validacion de campos
-    function validar(){
-
-      //expresion regular para comprobar que la contraseña tenga ciertos caracteres
-      $patronContraseña = "/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/";
-
-      //array de errores
-      $errores = array();
-
-      //validar que venga por post
-      if($_POST){
-
-      //validar que el usuario tenga mas de 8 caracteres
-      if( strlen($_POST["usuario"]) < 8 ){
-        $errores[] = "El campo del usuario debe tener más de 8 carácteres";
-      }
-
-      //validar el formato de email
-      if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false){
-        $errores[] = "El email no tiene el formato correcto";
-      }
-
-      //validar contraseña con expresion regular
-      if(!preg_match($patronContraseña, $_POST["contraseña"])){
-        $errores[] = "La contraseña debe tener entre 8 y 16 caracteres, al menos un número, al menos una minúscula y al menos una mayúscula.
-                      NO puede tener otros símbolos.";
-      }
-
-      //validar que las contraseñas sean iguales
-      if($_POST["contraseña"] != $_POST["repetirContraseña"]){
-        $errores[] = "Las contraseñas no coinciden";
-      }
-
-      //imprimo errores en un alerta o en un succes
-      if($errores>0){
-        echo "<br>";
-        for ($i=0; $i < count($errores); $i++) {
-          echo '<div class="alert alert-danger" role="alert">' . $errores[$i] . '</div>';
-        }
+  function validarUsuario(){
+    $patronUsuario = "/^[a-z0-9_-]{8,16}$/";
+    if($_POST){
+      //validar usuario con expresion regular
+      if(!preg_match($patronUsuario, $_POST["usuario"])){
+        global $errores;
+        $errores[] = "El nombre de usuario debe tener entre 8 y 16 carácteres, puede contener minúsculas, números, un guión bajo y/o guión medio";
+        $usuario = "";
       }
       else{
-        echo '<br><div class="alert alert-success" role="alert">Sus datos se han ingresado correctamente!</div>';
+        global $usuario;
+        $usuario = $_POST["usuario"];
       }
     }
   }
 
+  function validarEmail(){
+    if($_POST){
+      //validar el formato de email
+      if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) == false){
+        global $errores;
+        $errores[] = "El email no tiene el formato correcto";
+        $email = "";
+      }
+      else{
+        global $email;
+        $email = $_POST["email"];
+      }
+    }
+  }
 
-//}
+  function validarContraseña(){
+    $patronContraseña = "/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/";
+    if($_POST){
+      //validar contraseña con expresion regular
+      if(!preg_match($patronContraseña, $_POST["contraseña"])){
+        global $errores;
+        $errores[] = "La contraseña debe tener entre 8 y 16 caracteres, al menos un número, al menos una minúscula y al menos una mayúscula.
+                      NO puede tener otros símbolos.";
+        $contraseña = "";
+      }
+
+      //validar que las contraseñas sean iguales
+      elseif($_POST["contraseña"] != $_POST["repetirContraseña"]){
+        global $errores;
+        $errores[] = "Las contraseñas no coinciden";
+      }
+    }
+  }
+
+  function mostrarErrores(){
+    global $errores;
+    echo '<br>';
+    //imprimo errores en un alerta o en un succes
+    if($errores>0){
+      for ($i=0; $i < count($errores); $i++) {
+        echo '<div class="alert alert-danger" role="alert">' . $errores[$i] . '</div>';
+      }
+    }
+  }
+
+  function validarArchivos(){
+    if($_FILES){
+      global $errores;
+      //no tienen que haber errores registrados
+      if (empty($errores)) {
+        if ($_FILES["imagen"]["error"] != 0) {
+          $errores[] = "La imagen de perfil no se ha subido correctamente";
+        }
+        else{
+          $extension = pathinfo($_FILES["imagen"]["name"], PATHINFO_EXTENSION);
+          if ($extension != "jpg" && $extension != "jpeg" && $extension != "png") {
+            $errores[] = "La imagen debe ser de tipo jpg, jpeg o png";
+          }
+          else{
+            move_uploaded_file($_FILES["imagen"]["tmp_name"], "files/imagen." . $extension);
+          }
+        }
+      }
+    }
+  }
+
+  function recordarUsuario(){
+    if($_POST){
+      global $errores;
+      $arrayUsuario = [];
+      $usuariosArray = [];
+      //tiene que estar checkeado el "recordarme" para que funcione
+      if(isset($_POST["recordarme"])){
+        //no tiene que haber errores (incluso al subir la foto de perfil)
+        if(empty($errores)){
+          $arrayUsuario["usuario"] = $_POST["usuario"];
+          $arrayUsuario["email"] = $_POST["email"];
+          //si pongo "contraseña" como clave en el json recibe caracteres extraños
+          $arrayUsuario["contrasenia"] = password_hash($_POST["contraseña"], PASSWORD_DEFAULT);
+          $usuarios = file_get_contents("usuarios.json");
+          $usuariosArray = json_decode($usuarios, true);
+          //si no valido el array_push me da un warning no reconociendo el usuariosArray vacio como un array
+          if($usuariosArray==null){
+            $usuariosArray[] = $arrayUsuario;
+          }
+          else{
+          array_push($usuariosArray, $arrayUsuario);
+          }
+          $usuariosFinal = json_encode($usuariosArray);
+          file_put_contents("usuarios.json", $usuariosFinal);
+        }
+      }
+    }
+  }
 
  ?>
 
@@ -89,26 +141,38 @@
         <div class="todo">
         <h2>Completá tus datos</h2><br>
         <div class="cuadro">
-          <form class="" action="registro.php" method="POST">
+          <?php
+          validarUsuario();
+          validarEmail();
+          validarContraseña();
+          validarArchivos();
+          recordarUsuario();
+          ?>
+          <form class="" action="registro.php" method="POST" enctype="multipart/form-data">
             <div class="row">
                 <div class="col-xs-12 col-md-6">
-                  <input type="text" class="form-control" name="usuario" value="<?= $usuario?>" placeholder="Usuario">
+                  <input type="text" class="form-control" name="usuario" value="<?=$usuario;?>" placeholder="Usuario">
                 </div>
                 <div class="col-xs-12 col-md-6">
-                  <input type="password" class="form-control" name="contraseña" value="<?= $contraseña?>" placeholder="Contraseña">
+                  <input type="password" class="form-control" name="contraseña" value="<?=$contraseña?>" placeholder="Contraseña">
                 </div>
                 <div class="col-xs-12 col-md-6">
-                  <input type="email" class="form-control" name="email" value="<?= $email?>" placeholder="E-mail">
+                  <input type="email" class="form-control" name="email" value="<?=$email;?>" placeholder="E-mail">
                 </div>
                 <div class="col-xs-12 col-md-6">
-                  <input type="password" class="form-control" name="repetirContraseña" value="<?= $repetirContraseña?>" placeholder="Repita la contraseña">
+                  <input type="password" class="form-control" name="repetirContraseña" value="<?=$contraseña?>" placeholder="Confirmar contraseña">
                 </div>
-                <div class="col-md-4">
-                  <button type="button" class="btn btn-secondary">Subí tu foto de perfil</button>
+                <div class=" col-md-6">
+                  <div class="imagenPerfil">
+                    <label for="fotoPerfil">Adjuntá tu foto de perfil</label>
+                    <input type="file" id="fotoPerfil" name="imagen">
+                  </div>
                 </div>
             </div>
         </div>
-        <?php validar();?>
+        <?php
+        mostrarErrores();
+        ?>
         <div class="boton">
           <input type="checkbox" name="recordarme" value="">Recordarme<br>
           <button type="submit" class="btn">Crear Cuenta</button>
